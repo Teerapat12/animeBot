e891875e50ea35caeac5d3c18c63ed1e52be78a4
Teerapat12 <teerapat.time12@gmail.com>
2018-06-09 09:51:09 +0700

    Initial commit

---
+++ index.py
@@ -0,0 +1,29 @@
+import os
+import importlib
+import threading
+
+# Init
+d = 'sitesAdapter'
+moduleNames = [d + "." + o for o in os.listdir(d)
+               if os.path.isdir(os.path.join(d, o)) and "__pycache__" not in o]
+
+modules = map(importlib.import_module, moduleNames)
+
+adapters = [m.adapter() for m in modules]
+
+# Should check for update every n minutes
+def checkUpdate():
+    adapters[0].getNewest()
+    # Step 1 call all the .getNewest(n) and getall the names of the movies.
+
+
+
+    # Step 2 Check if the newest if newer than the old one.
+    # Step 3 If it is, call the alertUser() function.
+
+
+def startChecking():
+  threading.Timer(10.0, checkUpdate).start()
+  print("Checking")
+
+startChecking()
