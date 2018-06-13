694e1b98d52a45136cbb82637ddc9e32ddf39566
Teerapat12 <teerapat.time12@gmail.com>
2018-06-13 23:32:10 +0700

    - Change Line API from LINE Notify to Line Bot.
    TODO: connect with DialogFlow

--- alertAdapter/lineApi.py
+++ alertAdapter/lineApi.py
@@ -1,20 +1,105 @@
+
 import requests
+import json
 
-url = 'https://notify-api.line.me/api/notify'
-token = 'Fnn01pWTPEV1ZBSbcg0pddcXvrJGxOeio9w2nmBW6Oy'
-headers = {'content-type':'application/x-www-form-urlencoded','Authorization':'Bearer '+token}
-
+url = 'https://api.line.me/v2/bot/message/push'
+#token = 'Fnn01pWTPEV1ZBSbcg0pddcXvrJGxOeio9w2nmBW6Oy'
+token = 'xThFevCj5CuPYVD5cOO3dlstprs8qWE1ZJV1V3QBwdmqf1xy+/axP+tQ4ZoijT51l2buIZbVxs9RfVjkIDLT13qecRLggnkJzIWCEwEVvqqq+Ke0YbucqNYfzod1pjci38BAZEUlfdGubmhjmTS0XwdB04t89/1O/w1cDnyilFU='
+headers = {
+  'Content-Type': 'application/json; charset=UTF-8',
+  'Authorization': 'Bearer '+ token
+  }
+myId = "U41e301775679179f8b254b947baf9836"
 msg = 'ทดสอบภาษาไทย'
 
+def sendData(data):
+    d = json.dumps({
+        "to": myId,
+        "messages": data
+    })
+    r = requests.post(url, headers=headers, data=d)  # ส่งข้อมูล
+
 def sendMessage(msg):
-    r = requests.post(url, headers=headers, data={'message': msg})
-    return True
+    sendData([{'type':'text','text':msg}])
 
 
 def sendImg(msg,img):
-    r = requests.post(url, headers=headers, data={'message': msg,'imageFullsize':img,'imageThumbnail':img})
-    return True
+    sendData([{'type':'text', 'text':msg},{'type':'image','originalContentUrl': img, 'previewImageUrl':img}])
+
 
 def sendEpisode(ep):
-    sendImg(ep.name + " episode " + ep.ep + " has arrived on "+ ep.webName, ep.img)
+    print("HEY")
+    sendImg(ep.name + " episode " + ep.ep + " has arrived on " + ep.webName, ep.img)
     sendMessage(ep.url + ep.link)
+    #sendData({"type":"flex","altText":strMsg,"contents":flexMsg},{"type":"text","text":ep.url+ep.link})
+
+
+def ep2Str(ep):
+    return ep.name+" episode " + str(ep.ep)
+
+def ep2Data(ep):
+    print(ep.link)
+    return {
+        "type": "bubble",
+        "hero": {
+            "type": "image",
+            "url": ep.img,
+            "size": "full",
+            "aspectRatio": "20:13",
+            "aspectMode": "cover",
+            "action": {
+                "type": "uri",
+                "uri": ep.url+ep.link
+            }
+        },
+        "body": {
+            "type": "box",
+            "layout": "vertical",
+            "contents": [
+                {
+                    "type": "text",
+                    "text": ep.name,
+                    "weight": "bold",
+                    "size": "xl"
+                },
+                {
+                    "type": "box",
+                    "layout": "baseline",
+                    "margin": "md",
+                    "contents": [
+
+                        {
+                            "type": "text",
+                            "text": "Episode "+str(ep.ep),
+                            "size": "sm",
+                            "color": "#999999",
+                            "margin": "md",
+                            "flex": 0
+                        }
+                    ]
+                }
+            ]
+        },
+        "footer": {
+            "type": "box",
+            "layout": "vertical",
+            "spacing": "sm",
+            "contents": [
+                {
+                    "type": "button",
+                    "style": "link",
+                    "height": "sm",
+                    "action": {
+                        "type": "uri",
+                        "label": "WATCH",
+                        "uri": ep.url+ep.link
+                    }
+                },
+                {
+                    "type": "spacer",
+                    "size": "sm"
+                }
+            ],
+            "flex": 0
+        }
+    }
